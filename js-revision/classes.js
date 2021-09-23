92731e8b2098b955bb1c12e8d18cf7b9ce6abfa1
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-09-22 22:28:50 -0300

    feat: Private property created. Besides, I created an static property and method.

--- js-revision/classes.js
+++ js-revision/classes.js
@@ -27,19 +27,32 @@ const r = new Rectangle();
 
 class Pig {
 	property = "my property";
-	constructor(mood = "happy") {
+	#hunger; // -> We use # to create a private property and/or method.
+	static staticProperty = 42; // -> We use the reserved word 'static' to define a static property and/or method.
+
+	constructor(mood = "happy", hunger = false) {
 		this.mood = mood;
+		this.#hunger = hunger;
 	}
 
 	speak() {
-		console.log(`I'm a ${this.mood} pig.`);
+		console.log(`I'm a ${this.mood} pig... ${this.#hunger ? 'with hungry!' : 'satisfied!'}`);
+	}
+
+	static eat() {
+		console.log("I'm eating " + (this.staticProperty > 1 ? this.staticProperty + " apples!" : this.staticProperty + " apple!"));
 	}
 }
 
+Pig.eat();
 const happy = new Pig("happy");
-console.log(happy);
+//console.log(happy.__proto__);
+//console.log(happy.__proto__.speak);
 //happy.speak()
+//console.log(feliz);
+
 const sad = new Pig("sad");
 //sad.speak()
+
 const noMood = new Pig();
 //noMood.speak()
