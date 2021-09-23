4a94bf939a0813fbdbe751c034cd4b6d279e7fa5
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-09-23 16:43:27 -0300

    feat: Inline styles added to componentes. Revising destructuring again. We can create inline styles with objects or with functions that returns an object.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 
+const style2 = {
+  boxShadow: '0 5px 3px rgba(0,0,0,0.5)'
+}
+
+const style = ({ bg = '#222' }) => ({
+  backgroundColor: bg,
+  color: '#fff',
+  padding: '10px 15px',
+  margin: '10px 15px'
+});
+
 const Li = ({ children }) => {
   return (
-    <li className="li-class">{ children }</li>
+    <li style={{ ...style2, ...style({ bg: '#333' }) }} className="li-class">{ children }</li>
   )
 };
 
 const App = () => {
   const mood = "Sad";
   return (
-    <ul className="css-class">
+    <ul style={ style({ bg: '#750' }) } className="css-class">
       <Li mood="Happy">li value</Li>
     </ul>
   );
