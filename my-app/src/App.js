4aab4a741a2e87305fcbe99287b5fa3967959a82
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-09-23 16:35:22 -0300

    feat: Editing App component to test it.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const Li = ({ children }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <li className="li-class">{ children }</li>
+  )
+};
+
+const App = () => {
+  const mood = "Sad";
+  return (
+    <ul className="css-class">
+      <Li mood="Happy">li value</Li>
+    </ul>
   );
 }
 
