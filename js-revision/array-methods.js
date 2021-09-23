161e9414c0e83a948f02809884f9f4a49cb82206
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-09-22 23:18:44 -0300

    feat: Map method learned.

--- js-revision/array-methods.js
+++ js-revision/array-methods.js
@@ -8,3 +8,18 @@ const r = arr.filter((el, i) => {
 });
 //console.log(r);
 
+// Map method
+// It takes all the elements inside an array and applies on them a function. Finally, it returns an array of the same length but with a function applied to each element.
+
+//const mapped = arr.map((el, i) => `<h1>${el}</h1>`);
+//console.log(mapped);
+
+const users = [
+	{ id: 1, name: "Happy Pig" },
+	{ id: 2, name: "Sad Pig" },
+	{ id: 3, name: "Excited Pig" }, 
+	{ id: 4, name: "Felipe" },
+]
+
+const mapped = users.map((user, i) => `<h1>${user.name}</h1>`);
+console.log(mapped);
