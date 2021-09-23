d57d28c30374b0d1d90e8cad471713937f958253
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-09-23 16:34:17 -0300

    feat: Commenting my components to work with App component.

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,60 +1,60 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-//ReactDOM.render(
-  //<React.StrictMode>
-    //<App />
-  //</React.StrictMode>,
-  //document.getElementById('root')
-//);
-
-const Li = ({ children, mood, age, sex, home }) => {
-  console.log(age, home);
-
-  let pronoun;
-  if (sex.toLowerCase() == 'm') {
-    pronoun = 'He';
-  } else {
-    pronoun = 'She';
-  }
-  
-  return (
-    <li>{ children } is { mood }. { pronoun } has { age } years old.</li>
-  )
-}
-
-const X = () => // Functional component
-  <ul>
-    <Li 
-      mood={ "happy" }
-      age={ 24 }
-      sex={ 'm' }
-      home={ true }
-    >
-      Pig A
-    </Li>
-    <Li 
-      mood={ "sad" }
-      age={ 33 }
-      sex={ 'f' }
-      mood={ false }
-    >
-      Pig B
-    </Li>
-    <Li
-      mood={ "excited" }
-      sex={ 'm' }
-    >
-      Felipe
-    </Li>
-  </ul>
-
 ReactDOM.render(
- <X />, document.getElementById('root')
-)
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+//const Li = ({ children, mood, age, sex, home }) => {
+  //console.log(age, home);
+
+  //let pronoun;
+  //if (sex.toLowerCase() == 'm') {
+    //pronoun = 'He';
+  //} else {
+    //pronoun = 'She';
+  //}
+  
+  //return (
+    //<li>{ children } is { mood }. { pronoun } has { age } years old.</li>
+  //)
+//}
+
+//const X = () => // Functional component
+  //<ul>
+    //<Li 
+      //mood={ "happy" }
+      //age={ 24 }
+      //sex={ 'm' }
+      //home={ true }
+    //>
+      //Pig A
+    //</Li>
+    //<Li 
+      //mood={ "sad" }
+      //age={ 33 }
+      //sex={ 'f' }
+      //mood={ false }
+    //>
+      //Pig B
+    //</Li>
+    //<Li
+      //mood={ "excited" }
+      //sex={ 'm' }
+    //>
+      //Felipe
+    //</Li>
+  //</ul>
+
+//ReactDOM.render(
+ //<X />, document.getElementById('root')
+//)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
