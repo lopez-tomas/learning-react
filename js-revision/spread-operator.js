a4bb0e2c0fae882495b96070d560379d0f70ee84
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-09-22 22:44:56 -0300

    feat: Copying objects into objects.

--- js-revision/spread-operator.js
+++ js-revision/spread-operator.js
@@ -1,4 +1,4 @@
-const fn = (a, b, c) => console.log(a + b + c);
+const fn = (a, b, c) => console.log(a, b, c);
 
 const arr = [1, 2, 3, 4];
 
@@ -12,4 +12,25 @@ const arr1 = [5, 6]
 const arr2 = [...arr, 2, 3];
 const arr3 = arr; // -> This is NOT a copy of arr
 arr[0] = "happy pig";
-console.log(arr, arr2, arr3);
+//console.log(arr, arr2, arr3);
+
+const obj1 = { a: 1, b: 2 };
+const obj2 = { b: 5, c: "happy pig" };
+
+const obj3 = {...obj1}; // -> obj3 copies obj1
+obj1.a = 10;
+//console.log(obj3, obj1);
+
+const obj4 = {...obj1, ...obj2};
+console.log(obj4);
+
+const obj5 = {
+	...obj1,
+	loading: true,
+	data: {
+		prop: "lala",
+		animal: "doggy"
+	}
+}
+
+console.log(obj5);
