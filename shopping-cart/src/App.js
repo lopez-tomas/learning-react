f8dc1a00fc857d8774715e5adeed3693f7ccae13
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-11-12 17:13:05 -0300

    feat: importing & using Layout component.

--- shopping-cart/src/App.js
+++ shopping-cart/src/App.js
@@ -1,5 +1,6 @@
 import { Component } from 'react'
 import Products from './components/Products'
+import Layout from './components/Layout'
 
 class App extends Component {
   state = {
@@ -12,10 +13,12 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Products
-          add_to_cart={() => console.log('It does nothing')}
-          products={this.state.products}
-        />
+        <Layout>
+          <Products
+            add_to_cart={() => console.log('It does nothing')}
+            products={this.state.products}
+          />
+        </Layout>
       </div>
     )
   }
