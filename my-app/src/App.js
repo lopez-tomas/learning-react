0cbc48ecba8203591d434b7c42937740db598312
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-11-12 16:11:13 -0300

    feat: Methods or Properties?

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -17,9 +17,26 @@ class App extends Component {
 		last_name: '',
 	}
 
-	updateValues = (prop, value) => {
+	constructor(props) {
+		super(props)
+		this.updateFirstName = this.updateFirstName.bind(this)
+	}
+	// Third fix: Use this with
+
+	//updateFirstName(value) {
+		//this.updateValues('first_name', value.target.value)
+	//}
+
+	updateFirstName = (value) => {
+		this.updateValues('first_name', value.target.value)
+	}
+
+	updateValues(prop, value) {
 		this.setState({ [prop]: value })
 	}
+	//updateValues = (prop, value) => { First fix: Using properties with functions
+		//this.setState({ [prop]: value })
+	//}
 
 	render() {
 		return (
@@ -27,7 +44,9 @@ class App extends Component {
 				Full name: {`${this.state.first_name} ${this.state.last_name}`}
 				<Input 
 					value={this.state.first_name}
-					onChange={e => this.updateValues('first_name', e.target.value)}
+					onChange={this.updateFirstName} // Third fix
+					//onChange={(arg) => this.updateFirstName(arg)} // Second fix: When we are using methods instead of properties.
+					//onChange={e => this.updateValues('first_name', e.target.value)} // First fix
 				/>
 				<Input 
 					value={this.state.last_name}
