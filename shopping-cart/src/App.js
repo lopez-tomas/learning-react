b3a9f34d18007fd19a623168d40be2e971fe9be2
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-11-12 18:46:24 -0300

    feat: adding products to cart.

--- shopping-cart/src/App.js
+++ shopping-cart/src/App.js
@@ -10,16 +10,40 @@ class App extends Component {
       { name: 'Tomato', price: 1500, img: '/products/tomato.jpg' },
       { name: 'Peas', price: 2500, img: '/products/peas.jpg' },
       { name: 'Lettuce', price: 500, img: '/products/lettuce.jpg' },
-    ]
+    ],
+    cart: [
+      //{ name: 'Tomato', price: 1500, img: '/products/tomato.jpg', amount: 1 },
+    ],
   }
+
+  add_to_cart = (product) => {
+    const { cart } = this.state
+    if (cart.find(x => x.name === product.name)) {
+      const new_cart = cart.map(x => x.name === product.name
+        ? ({
+          ...x,
+          amount: x.amount + 1,
+        })
+        : x)
+      return this.setState({ cart: new_cart })
+    }   
+    return this.setState({
+      cart: this.state.cart.concat({
+        ...product,
+        amount: 1,
+      })
+    })
+  }
+
   render() {
+    console.log(this.state.cart)
     return (
       <div>
         <Navbar />
         <Layout>
           <Title />
           <Products
-            add_to_cart={() => console.log('It does nothing')}
+            add_to_cart={this.add_to_cart}
             products={this.state.products}
           />
         </Layout>
