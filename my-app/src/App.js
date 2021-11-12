3e838a8457e93a9e572bdbb6895d1e112ba0cb85
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-11-12 13:34:56 -0300

    feat: Conditional rendering.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,14 @@
 import Button from "./Button"
 
 const App = () => {
+	const my_variable = true
+	  
+	if (my_variable) {
+		return <p>My variable gave true!</p>
+	}
 	return (
 		<div>
-			<h1>Hello, world</h1>
+			<h1 onClick={(e) => console.log('click', e)}>Hello, world</h1>
 			<Button onClick={() => console.log('clicked')}>
 				Send
 			</Button>
