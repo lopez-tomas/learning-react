afe921d977f46abe5424a7262141fa6218ab7506
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-11-12 16:55:33 -0300

    feat: in-line styles for Products component added.

--- shopping-cart/src/components/Products.js
+++ shopping-cart/src/components/Products.js
@@ -1,12 +1,20 @@
 import { Component } from 'react'
 import Product from './Product'
 
+const styles = {
+	products: {
+		display: 'flex',
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+	}
+}
+
 class Products extends Component {
 	render() {
 		const { products, add_to_cart } = this.props
 
 		return (
-			<div>
+			<div style={styles.products}>
 				{products.map(product => 
 					<Product
 						add_to_cart={add_to_cart}
