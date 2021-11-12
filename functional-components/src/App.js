6a3849aa760919444518578c36a66783b3446b13
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-11-12 20:19:49 -0300

    feat: Creating first custom hook.

--- functional-components/src/App.js
+++ functional-components/src/App.js
@@ -1,4 +1,4 @@
-import { Component, useState } from 'react'
+import { useState } from 'react'
 
 // Hooks' rules:
 // 1) They can't be called on conditions nor loops. Always at the higher level of the component.
@@ -8,29 +8,46 @@ import { Component, useState } from 'react'
 // 3) When we create a Custom Hook, it must always start with use*(* being hook name)
 
 
-class App extends Component {
-	state = { counter: 0 }
-	increment = () => {
-		this.setState({ counter: this.state.counter + 1 })
+//class App extends Component {
+	//state = { counter: 0 }
+	//increment = () => {
+		//this.setState({ counter: this.state.counter + 1 })
+	//}
+	//render() {
+		//return (
+			//<div>
+				//Counter: {this.state.counter}
+				//<button onClick={this.increment}>Increment</button>
+			//</div>
+		//)
+	//}
+//}
+
+const useCounter = (initial) => {
+	const [counter, set_counter] = useState(initial)
+
+	const increment = () => {
+	  set_counter(counter + 1)
 	}
-	render() {
-		return (
-			<div>
-				Counter: {this.state.counter}
-				<button onClick={this.increment}>Increment</button>
-			</div>
-		)
+
+	const decrement = () => {
+		if (counter !== 0) {
+	  	set_counter(counter - 1)
+		}
 	}
+
+	return [counter, increment, decrement]
 }
 
-//const App = () => {
-	//const [counter, set_counter] = useState(0)
-	//return (
-		//<div>
-			//Counter: {counter}
-			//<button onClick={() => set_counter(counter + 1)}>Increment</button>
-		//</div>
-	//)
-//}
+const App = () => {
+	const [counter, increment, decrement] = useCounter(0)
+	return (
+		<div>
+			Counter: {counter}
+			<button onClick={increment}>Increment</button>
+			<button onClick={decrement}>Decrement</button>
+		</div>
+	)
+}
 
 export default App
