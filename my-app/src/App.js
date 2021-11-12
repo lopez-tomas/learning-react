beeaf69b9770287ec27d6b3b44fc5c5067340dbf
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-11-12 13:59:13 -0300

    feat: Learning about 'state'.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,21 @@
 import { Component } from 'react'
 
-class App extends Component {
+class App extends Component { // we must extend 'Component' to use 'setState'
+	state = { // it must ALWAYS be called 'state'
+		value: 3
+	}
 	render() {
+		console.log(this.state);
 		return (
-			<p>
-				Hello, World!
-			</p>
+			<div>
+				<p>Hello, World!</p>
+				<button 
+					className={`${this.state.value}`} 
+					onClick={() => this.setState({  value: 2 })} // refresh 'state'
+				>
+					Send
+				</button>
+			</div>
 		)
 	}
 }
