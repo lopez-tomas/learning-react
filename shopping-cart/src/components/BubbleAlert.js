ddca85e99a52399013770b0be6ab84b3849862ea
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-11-12 18:33:55 -0300

    feat: getting number of items in cart for BubbleAlert component.

--- shopping-cart/src/components/BubbleAlert.js
+++ shopping-cart/src/components/BubbleAlert.js
@@ -12,10 +12,17 @@ const styles = {
 }
 
 class BubbleAlert extends Component {
+	getNumber(n) {
+		if (!n) { return ' ' }
+		return n > 9 ? '9+' : n
+	}
+
 	render() {
+		const { value } = this.props
+
 		return (
 			<span style={styles.bubble_alert}>
-				5
+				{this.getNumber(value)}
 			</span>
 		)
 	}
