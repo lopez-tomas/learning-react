ab4c6c89cb183a4e9fcb71bc7923eaee81e08642
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-11-12 15:49:23 -0300

    feat: new App.js file.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,53 +1,29 @@
 import { Component } from 'react'
 
-class Button extends Component {
-	state = {}
-	constructor(props) {
-		super(props)
-		console.log('constructor', props);
-	}
+class Input extends Component {
+	state = { value: '' }
 
-	componentDidMount() {
-		console.log('componentDidMount');
+	handleChange = (value) => {
+		this.setState({ value: value })
 	}
-
-	componentDidUpdate(prevProp, prevState) {
-		console.log('componentDidUpdate', prevProp, prevState);
-	}
-
-	componentWillUnmount() {
-		console.log('componentWillUnmount', this.props, this.state);
-	}
-
 	render() {
-		console.log('running render method of button');
 		return (
-			<button onClick={() => this.setState({ prop: 1 })}>
-				Send	
-			</button>
+			<input
+				value={this.state.value}
+				onChange={e => this.handleChange(e.target.value)}
+			/>
 		)
 	}
 }
 
-class App extends Component { // we must extend 'Component' to use 'setState'
-	state = { // it must ALWAYS be called 'state'
-		value: 3
-	}
+class App extends Component {
 	render() {
-		console.log(this.state);
 		return (
-			<div>
-				<p>Hello, World!</p>
-				{this.state.value === 3
-					? <Button piggy="happy"/>
-					: null}
-				<button 
-					className={`${this.state.value}`} 
-					onClick={() => this.setState({  value: 2 })} // refresh 'state'
-				>
-					Send on App
-				</button>
-			</div>
+			<p>
+				Full name:
+				<Input />
+				<Input />
+			</p>
 		)
 	}
 }
