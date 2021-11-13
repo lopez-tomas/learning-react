98c354f7c0f9ea7e02f456233b02e5aaf7fef98a
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-11-12 22:21:44 -0300

    feat: useRef() learned.

--- functional-components/src/App.js
+++ functional-components/src/App.js
@@ -1,36 +1,23 @@
-import { useReducer, useState } from 'react'
-
-// const state = { counter: 0 }
-// action = { type: string, payload: any }
-const initial = { counter: 0 }
-
-const reducer = (state, action) => {
-	switch(action.type) {
-		case 'increment':
-			return { counter: state.counter + 1 }
-		case 'decrement':
-			if (state.counter !== 0) {
-				return { counter: state.counter - 1 }
-			}
-			return state
-		case 'set':
-			return { counter: action.payload }
-		default:
-			return state
-	}
-}
+import { useRef } from 'react'
 
 const App = () => {
-	const [state, dispatch] = useReducer(reducer, initial)
-	const [value, set_value] = useState(0)
+	const ref = useRef()
+	const input_ref = useRef()
+	const click = () => {
+		console.log(ref.current.clientHeight)
+		ref.current.innerHTML = 'Happy piggy'
+	}
+
+	const focus = () => {
+	  input_ref.current.focus()
+		ref.current.innerHTML = 'Lala'
+	};
 
 	return (
 		<div>
-			Counter: {state.counter}
-			<input value={value} onChange={e => set_value(Number(e.target.value))}/>
-			<button onClick={() => dispatch({ type: 'increment' })}>Increment</button>
-			<button onClick={() => dispatch({ type: 'decrement' })}>Decrement</button>
-			<button onClick={() => dispatch({ type: 'set', payload: value })}>Set</button>
+			<input ref={input_ref} />
+			<button onClick={focus}>Focus</button>
+			<div onClick={click} ref={ref}>Lala</div>
 		</div>
 	)
 }
