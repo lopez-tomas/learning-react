77f96ffa05403e882f9b06019c79eba08b2d5394
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-11-14 18:22:21 -0300

    feat: Uncontrolled components using useRef()

--- forms/src/App.js
+++ forms/src/App.js
@@ -1,21 +1,27 @@
-function App() {
-  const submit = (e) => {
-    e.preventDefault()
-    const data = Array.from(new FormData(e.target))
-    console.log(Object.fromEntries(data))
-  };
-  return (
-    <form onSubmit={submit}>
-      <div>
-        <span>
-          lala
-        </span>
-        <input name="field" />
-      </div>
-      <input name="field-2" />
-      <input type="submit" value="Send"/>
-    </form>
-  )
+import { useRef } from 'react'
+
+const App = () => {
+	const input = useRef() // I must create a reference for each field of my form
+	const file = useRef() // I must create a reference for each field of my form
+	const submit = () => {
+		console.log(input.current.value)
+		console.log(file.current.files[0])
+		const form = new FormData()
+		form.append('file', file.current.files[0])
+		form.append('field', input.current.value)
+		fetch('/lala', { method: "POST", body: form })
+	}
+
+	return (
+		<div>
+			<div>
+				<span>Lala</span>
+				<input type="text" name="field" ref={input}/>
+				<input type="file" ref={file}/>
+			</div>
+			<input type="submit" value="Send" onClick={submit}/>
+		</div>
+	)
 }
 
 export default App
