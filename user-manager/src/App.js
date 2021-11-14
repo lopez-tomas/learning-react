0cdae3094fb377dff8581cef76701d96f0c37121
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-11-14 20:05:30 -0300

    feat: Using Input component.

--- user-manager/src/App.js
+++ user-manager/src/App.js
@@ -1,17 +1,17 @@
 import useForm from './hooks/useForm'
+import Input from './components/Input'
 
 function App() {
-  const [form, handleChange] = useForm({ name: '' })
+  const [form, handleChange] = useForm({
+    name: '',
+    last_name: '',
+  })
 
   console.log(form)
   return (
     <form>
-      <input 
-        name="name" 
-        placeholder="Name" 
-        value={form.name}
-        onChange={handleChange} 
-      />
+      <Input label="Name" name="name" value={form.name} onChange={handleChange} />
+      <Input label="Last name" name="last_name" value={form.last_name} onChange={handleChange} />
     </form>
   )
 }
