af21d7f9020d73805e5d55fb4dfe241ddbfa410f
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-11-14 18:56:02 -0300

    feat: Controlled components - Synchronizing multiple values.

--- forms/src/App.js
+++ forms/src/App.js
@@ -1,16 +1,26 @@
 import { useState } from 'react'
 
 const App = () => {
-	const [value, setValue] = useState('')
+	const [value, setValue] = useState({ normal: '' , text: ''})
 	const handleChange = (e) => {
-	  setValue(e.target.value)
+		// 1st solution to maintain state object
+		setValue((state) => ({
+			...state,
+			[e.target.name]: e.target.value
+		}))
+		// 2nd solution to maintain state object
+		//setValue({ 
+			//...value,
+			//[e.target.name]: e.target.value 
+		//})
 	}
 	console.log(value)
 
 	return (
 		<div>
 			{value.length < 5 ? <span>Minimun length is 5</span> : null}
-			<input type="text" name="normal" value={value} onChange={handleChange} />
+			<input type="text" name="normal" value={value.normal} onChange={handleChange} placeholder="Normal" />
+			<textarea name="text" value={value.text} onChange={handleChange} placeholder="Write here..."/>
 		</div>
 	)
 }
