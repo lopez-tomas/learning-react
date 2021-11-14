60b37acd54bc22b2041c417ff2fc794d170f72da
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-11-14 18:21:54 -0300

    feat: App.js renamed to uncontrolled-component.js

---
+++ forms/src/uncontrolled-component.js
@@ -0,0 +1,21 @@
+function App() {
+  const submit = (e) => {
+    e.preventDefault()
+    const data = Array.from(new FormData(e.target))
+    console.log(Object.fromEntries(data))
+  };
+  return (
+    <form onSubmit={submit}>
+      <div>
+        <span>
+          lala
+        </span>
+        <input name="field" />
+      </div>
+      <input name="field-2" />
+      <input type="submit" value="Send"/>
+    </form>
+  )
+}
+
+export default App
