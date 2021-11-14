7aff66fdc2d11533b8f45073007778028b4a26ec
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-11-14 20:39:58 -0300

    feat: minor changes.

--- user-manager/src/App.js
+++ user-manager/src/App.js
@@ -1,6 +1,9 @@
 import { useState } from 'react'
 
+// Hooks
 import useForm from './hooks/useForm'
+
+// Components
 import Input from './components/Input'
 import Card from './components/Card'
 import Container from './components/Container'
@@ -24,39 +27,44 @@ function App() {
   };
 
   return (
-    <Container>
-      <Card>
-        <div style={{ padding: 20 }}>
-          <form onSubmit={submit}>
-            <Input
-              label="Name" 
-              name="name"
-              value={form.name}
-              onChange={handleChange}
-            />
-            <Input
-              label="Last name"
-              name="last_name"
-              value={form.last_name}
-              onChange={handleChange}
-            />
-            <Input
-              label="Email"
-              name="email"
-              value={form.email}
-              onChange={handleChange}
-            />
-            <Button>Send</Button>
-          </form>
-        </div>
-      </Card>
-      <Card>
-        <ul>
-          {users.map(user =>
-          <li key={user.email}>{`${user.name} ${user.last_name}: ${user.email}`}</li>)}
-        </ul>
-      </Card>
-    </Container>
+    <div style={{ marginTop: '15%' }}>
+      <Container>
+        <Card>
+          <div style={{ padding: 20 }}>
+            <form onSubmit={submit}>
+              <Input
+                label="Name" 
+                name="name"
+                value={form.name}
+                onChange={handleChange}
+                placeholder="Name"
+              />
+              <Input
+                label="Last name"
+                name="last_name"
+                value={form.last_name}
+                onChange={handleChange}
+                placeholder="Last name"
+              />
+              <Input
+                label="Email"
+                name="email"
+                value={form.email}
+                onChange={handleChange}
+                placeholder="Email"
+              />
+              <Button>Send</Button>
+            </form>
+          </div>
+        </Card>
+        <Card>
+          <ul>
+            {users.map(user =>
+            <li key={user.email}>{`${user.name} ${user.last_name}: ${user.email}`}</li>)}
+          </ul>
+        </Card>
+      </Container>
+    </div>
   )
 }
 
