169bf21452d8aa01a26f1b8095a470f599eae479
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-11-14 19:51:12 -0300

    feat: First steps & using custom hook useForm().

--- user-manager/src/App.js
+++ user-manager/src/App.js
@@ -1,5 +1,18 @@
+import useForm from './hooks/useForm'
+
 function App() {
+  const [form, handleChange] = useForm({ name: '' })
+
+  console.log(form)
   return (
+    <form>
+      <input 
+        name="name" 
+        placeholder="Name" 
+        value={form.name}
+        onChange={handleChange} 
+      />
+    </form>
   )
 }
 
