5b7c207523539dfd88a6ef74c79db7ade79047c2
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-11-15 12:53:55 -0300

    feat: max-width style added.

--- compound-interest/src/components/Section.js
+++ compound-interest/src/components/Section.js
@@ -4,7 +4,8 @@ const Section = styled.section`
   background-color: #eee;
   border-top: solid 2px palevioletred;
   padding: 20px 25px;
-  width: 500px;
+  width: 80%;
+  max-width: 500px;
   box-shadow: 0px 2px 3px rgb(0, 0, 0, 0.3);
 `
 export default Section
