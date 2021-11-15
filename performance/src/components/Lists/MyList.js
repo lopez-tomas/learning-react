f04d3c5727a5d703dbfa1e9eb7fa60e921130c87
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-11-15 14:00:03 -0300

    feat: memo with 'children' properties. MyList component is now using memo().

--- performance/src/components/Lists/MyList.js
+++ performance/src/components/Lists/MyList.js
@@ -1,13 +1,37 @@
 import { memo } from 'react'
+import { isEqual } from 'lodash'
 
-const Li = memo(({ fullname }) => {
-  console.log(`rendering ${fullname}`)
+//const Li = memo(({ fullname }) => {
+  //console.log(`rendering ${fullname}`)
+  //return (
+    //<li>
+      //{fullname}
+    //</li>
+  //)
+//})
+
+const Li = memo(({ children }) => {
+  console.log(`rendering ${children}`)
   return (
     <li>
-      {fullname}
+      {children}
     </li>
   )
-})
+}, isEqual)
+
+//const MyList = ({ data }) => {
+  //console.log('rendering list')
+  //return (
+    //<ul>
+      //{data.map(x =>
+        //<Li
+          //key={x.name + x.lastname}
+          //fullname={`${x.name} ${x.lastname}`}
+        ///>
+      //)}
+    //</ul>
+  //)
+//}
 
 const MyList = ({ data }) => {
   console.log('rendering list')
@@ -16,11 +40,12 @@ const MyList = ({ data }) => {
       {data.map(x =>
         <Li
           key={x.name + x.lastname}
-          fullname={`${x.name} ${x.lastname}`}
-        />
+        >
+          {x.name} {x.lastname}
+        </Li>
       )}
     </ul>
   )
 }
 
-export default MyList
+export default memo(MyList)
