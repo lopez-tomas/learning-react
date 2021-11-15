19c2afecf8ed8b732014ebe40238b6324c5cc0b6
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-11-15 00:48:51 -0300

    feat: importing Formik, Form & Field from Formik. Header built.

--- image-search-engine/src/App.js
+++ image-search-engine/src/App.js
@@ -1,5 +1,23 @@
+import { Formik, Form, Field } from 'formik'
+import './header.css'
+
 const App = () => {
   return (
+    <div>
+      <header>
+        <Formik
+          initialValues={{ search: '' }}
+          onSubmit={async values => {
+            // unsplash API calling
+            console.log(values)
+          }}
+        >
+          <Form>
+            <Field name="search" />
+          </Form>
+        </Formik>
+      </header>
+    </div>
   )
 }
 
