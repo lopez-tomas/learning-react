0afb80fe23ed7fd2c83978d0ae619deaeb7d17a7
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-11-14 22:12:30 -0300

    feat: App.js renamed to useFormik.js - Here we use Formik to create a form.

---
+++ formik/src/useFormik.js
@@ -0,0 +1,59 @@
+import { useFormik } from 'formik'
+
+const validate = (values) => {
+  const errors = {}
+
+  if(!values.name) {
+    errors.name = 'Required'
+  } else if (values.name.length < 5) {
+    errors.name = 'The name is too short.'
+  }
+
+  if(!values.last_name) {
+    errors.last_name = 'Required'
+  } else if (values.last_name.length < 5) {
+    errors.last_name = 'The last name is too short.'
+  }
+
+  return errors
+}
+
+function App() {
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      last_name: '',
+      email: '',
+    },
+    validate, 
+    onSubmit: values => console.log(values),
+  })
+
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <label>Name</label>
+      <input type="text" {...formik.getFieldProps('name')}/>
+      {formik.touched.name && formik.errors.name
+        ? <div>{formik.errors.name}</div>
+        : null
+      }
+      <br />
+      <label>Last name</label>
+      <input type="text" {...formik.getFieldProps('last_name')}/>
+      {formik.touched.last_name && formik.errors.last_name
+        ? <div>{formik.errors.last_name}</div>
+        : null
+      }
+      <br />
+      <label>Email</label>
+      <input type="email" {...formik.getFieldProps('email')}/>
+      {formik.touched.email && formik.errors.email
+        ? <div>{formik.errors.email}</div>
+        : null
+      }
+      <button type="submit">Send</button>
+    </form>
+  )
+}
+
+export default App
