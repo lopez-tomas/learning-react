1aedc8768a1c85f02f06463ac7c2938dc82b34b8
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-11-15 14:47:52 -0300

    feat: createContext & useContext imported from React. Introduction to Context.

--- context/src/App.js
+++ context/src/App.js
@@ -1,6 +1,36 @@
+import { createContext, useContext } from 'react'
+
+const ContextDefault = createContext('default value')
+const Context2 = createContext('default value 2')
+
+const DefaultProvider = ({ children }) => {
+  return (
+    <ContextDefault.Provider value={'my value'}>
+      {children}
+    </ContextDefault.Provider>
+  )
+}
+
+const Content = () => {
+  const ctx = useContext(ContextDefault)
+  return (
+    <div>{ctx}</div>
+  )
+}
+
+const Content2 = () => {
+  const ctx = useContext(Context2)
+  return (
+    <div>{ctx}</div>
+  )
+}
+
 const App = () => {
   return (
-    <div>Hello, World!</div>
+    <DefaultProvider>
+      <Content />
+      <Content2 />
+    </DefaultProvider>
   )
 }
 
