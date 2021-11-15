346d4b2412206494938296ab08c3dbfc1ee6dfe2
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-11-14 21:57:22 -0300

    feat: validation is triggered when field is touched.

--- formik/src/App.js
+++ formik/src/App.js
@@ -36,26 +36,33 @@ function App() {
         name="name"
         type="text"
         onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
         value={formik.values.name}
       />
-      {formik.errors.name ? <div>{formik.errors.name}</div> : null}
+      {formik.touched.name && formik.errors.name ?
+        <div>{formik.errors.name}</div> : null}
       <br />
       <label>Last name</label>
       <input
         name="last_name"
         type="text"
         onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
         value={formik.values.last_name}
       />
-      {formik.errors.last_name ? <div>{formik.errors.last_name}</div> : null}
+      {formik.touched.last_name && formik.errors.last_name ?
+        <div>{formik.errors.last_name}</div> : null}
       <br />
       <label>Email</label>
       <input
         name="email"
         type="email"
         onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
         value={formik.values.email}
       />
+      {formik.touched.email && formik.errors.email ?
+        <div>{formik.errors.email}</div> : null}
       <button type="submit">Send</button>
     </form>
   )
