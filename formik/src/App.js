f04562ea458bd36fb1976a4b313c8570bf945490
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-11-14 21:38:06 -0300

    feat: introduction to Formik library.

--- formik/src/App.js
+++ formik/src/App.js
@@ -1,5 +1,40 @@
+import { useFormik } from 'formik'
+
 function App() {
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      last_name: '',
+      email: '',
+    },
+    onSubmit: values => console.log(values),
+  })
+
   return (
+    <form onSubmit={formik.handleSubmit}>
+      <label>Name</label>
+      <input
+        name="name"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.name}
+      />
+      <label>Last name</label>
+      <input
+        name="last_name"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.last_name}
+      />
+      <label>Email</label>
+      <input
+        name="email"
+        type="email"
+        onChange={formik.handleChange}
+        value={formik.values.email}
+      />
+      <button type="submit">Send</button>
+    </form>
   )
 }
 
