8a4a183096100c99626d0e526cffe3267259db86
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-11-14 22:02:50 -0300

    feat: eliminating repetitive code with Formik using getFieldProps()

--- formik/src/App.js
+++ formik/src/App.js
@@ -32,37 +32,25 @@ function App() {
   return (
     <form onSubmit={formik.handleSubmit}>
       <label>Name</label>
-      <input
-        name="name"
-        type="text"
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values.name}
-      />
-      {formik.touched.name && formik.errors.name ?
-        <div>{formik.errors.name}</div> : null}
+      <input type="text" {...formik.getFieldProps('name')}/>
+      {formik.touched.name && formik.errors.name
+        ? <div>{formik.errors.name}</div>
+        : null
+      }
       <br />
       <label>Last name</label>
-      <input
-        name="last_name"
-        type="text"
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values.last_name}
-      />
-      {formik.touched.last_name && formik.errors.last_name ?
-        <div>{formik.errors.last_name}</div> : null}
+      <input type="text" {...formik.getFieldProps('last_name')}/>
+      {formik.touched.last_name && formik.errors.last_name
+        ? <div>{formik.errors.last_name}</div>
+        : null
+      }
       <br />
       <label>Email</label>
-      <input
-        name="email"
-        type="email"
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values.email}
-      />
-      {formik.touched.email && formik.errors.email ?
-        <div>{formik.errors.email}</div> : null}
+      <input type="email" {...formik.getFieldProps('email')}/>
+      {formik.touched.email && formik.errors.email
+        ? <div>{formik.errors.email}</div>
+        : null
+      }
       <button type="submit">Send</button>
     </form>
   )
