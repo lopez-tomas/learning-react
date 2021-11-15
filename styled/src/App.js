9b099c41d0b746766960167c503ac86d769d11b2
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-11-15 10:40:07 -0300

    feat: introduction to Styled Components - P styled component created.

--- styled/src/App.js
+++ styled/src/App.js
@@ -1,5 +1,15 @@
+import styled from 'styled-components'
+
+const P = styled.p`
+  font-size: 24px;
+  color: red;
+`
+
 const App = () => {
   return (
+    <div>
+      <P>Hello, I'm a paragraph.</P>
+    </div>
   )
 }
 
