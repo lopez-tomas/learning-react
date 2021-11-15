b8fe64fd0fad589fdb55b8de40278eccb45c677e
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-11-15 00:22:14 -0300

    feat: console.log() eliminated.

--- formik/src/components/Checkbox.js
+++ formik/src/components/Checkbox.js
@@ -3,7 +3,6 @@ import { useField } from 'formik'
 const Checkbox = ( { children, ...props } ) => {
 	const [field, meta] = useField({ ...props, type: 'checkbox' })
 
-	console.log(field)
 	return (
 		<div>
 			<label>
