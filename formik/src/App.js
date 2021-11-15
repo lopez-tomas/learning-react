3d245b5ec523e1e264b5f1779e4e7efab4fd1ab2
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-11-14 22:18:29 -0300

    feat: using Field, Form & ErrorMessage components of Formik.

--- formik/src/App.js
+++ formik/src/App.js
@@ -1,4 +1,4 @@
-import { Formik } from 'formik'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
 
 const validate = (values) => {
   const errors = {}
@@ -25,31 +25,24 @@ function App() {
 			validate={validate}
 			onSubmit={values => console.log(values)}
 		>
-			{formik =>
-				<form onSubmit={formik.handleSubmit}>
-					<label>Name</label>
-					<input type="text" {...formik.getFieldProps('name')}/>
-					{formik.touched.name && formik.errors.name
-						? <div>{formik.errors.name}</div>
-						: null
-					}
-					<br />
-					<label>Last name</label>
-					<input type="text" {...formik.getFieldProps('last_name')}/>
-					{formik.touched.last_name && formik.errors.last_name
-						? <div>{formik.errors.last_name}</div>
-						: null
-					}
-					<br />
-					<label>Email</label>
-					<input type="email" {...formik.getFieldProps('email')}/>
-					{formik.touched.email && formik.errors.email
-						? <div>{formik.errors.email}</div>
-						: null
-					}
-					<button type="submit">Send</button>
-				</form>
-			}
+			<Form>
+				<label>Name</label>
+				<Field name="name" type="text" />
+				<ErrorMessage name="name" />
+				<br />
+
+				<label>Last name</label>
+				<Field name="last_name" type="text" />
+				<ErrorMessage name="last_name" />
+				<br />
+
+				<label>Email</label>
+				<Field name="email" type="email" />
+				<ErrorMessage name="email" />
+				<br />
+
+				<button type="submit">Send</button>
+			</Form>
 		</Formik>
   )
 }
