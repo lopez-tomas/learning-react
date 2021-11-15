425aec437d5c5aca8e9b75d2c9ae4fef6853bbee
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-11-15 14:08:17 -0300

    feat: useCallback imported from React and used in handleSubmit() function. MyForm functional component is now optimized.

--- performance/src/App.js
+++ performance/src/App.js
@@ -1,16 +1,16 @@
-import { useState } from 'react'
+import { useState, useCallback } from 'react'
 import Title from './components/Title'
 import MyForm from './components/Forms/MyForm'
 import MyList from './components/Lists/MyList'
 
 function App() {
   const [values, setValues] = useState([])
-  const handleSubmit = (val) => {
-    setValues([
-      ...values,
+  const handleSubmit = useCallback((val) => {
+    setValues(data => ([
+      ...data,
       val
-    ])
-  }
+    ]))
+  }, [])
 
   return (
     <div>
