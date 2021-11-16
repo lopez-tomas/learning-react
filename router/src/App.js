c3b4305c1479562433e3e3aa58099c3fe33d3480
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-11-16 00:32:30 -0300

    feat: 404 Error.

--- router/src/App.js
+++ router/src/App.js
@@ -1,26 +1,4 @@
-import { useEffect } from 'react'
-import { Routes, Route, Link, useNavigate } from 'react-router-dom'
-
-const Redirect = ({ to }) => {
-  let navigate = useNavigate()
-  useEffect(() => {
-    navigate(to)
-  })
-
-  return null
-};
-
-const Portfolio = () => {
-  const loggedIn = false
-
-  if (!loggedIn) {
-    return <Redirect path="/portfolio" push to="/" />
-  }
-
-  return (
-    <h1>Portfolio</h1>
-  )
-}
+import { Routes, Route, Link } from 'react-router-dom'
 
 const App = () => {
   return (
@@ -33,17 +11,13 @@ const App = () => {
           <li>
             <Link to="/perfil">Perfil</Link>
           </li>
-          <li>
-            <Link to="/portfolio">Portfolio</Link>
-          </li>
         </ul>
       </nav>
       <section>
         <Routes>
-          <Route path="/" element={<Redirect path="/" to="/home" />} />
-          <Route path="/home" element={<h1>Home</h1>} />
+          <Route path="/" element={<h1>Home</h1>} />
           <Route path="/perfil" element={<h1>Perfil</h1>} />
-          <Route path="/portfolio" element={<Portfolio />} />
+					<Route path="*" element={<p>404: path not found</p>} />
         </Routes>
       </section>
     </div>
