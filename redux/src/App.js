718446ceec49bb2872f83d3a182c71abb51f46be
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-11-20 01:27:04 -0300

    feat: returning a new copy of our initialState. We MUST NEVER do mutability in our reducers.

--- redux/src/App.js
+++ redux/src/App.js
@@ -1,13 +1,37 @@
-export const reducer = (state = {}, action) => {
+import { useDispatch, useSelector } from 'react-redux'
+const initialState = {
+	entities: [],
+}
+export const reducer = (state = initialState, action) => {
+	switch(action.type) {
+		case 'to-do/add': {
+			console.log('reducer!')
+			return {
+				...state,
+				entities: [{}]
+			}
+		}
+	}
 	return state;
 }
 
 const App = () => {
+	const dispatch = useDispatch()
+	const state = useSelector(x => x)
+	console.log(state, 'rendering')
+
 	return (
 		<div>
 			<form>
 				<input />
 			</form>
+			<button onClick={() => dispatch({ type: 'to-do/add' })}>Show TO-DOs</button>
+			<button>Completed</button>
+			<button>Uncompleted</button>
+			<ul>
+				<li>TO-DO 1</li>
+				<li>TO-DO 2</li>
+			</ul>
 		</div>
 	)
 }
