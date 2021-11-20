87a58884d5ec0695e3235698dacf6130e6366eb4
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-11-20 01:34:26 -0300

    feat: adding elements to our list.

--- redux/src/App.js
+++ redux/src/App.js
@@ -1,36 +1,52 @@
 import { useDispatch, useSelector } from 'react-redux'
+import { useState } from 'react'
 const initialState = {
 	entities: [],
 }
 export const reducer = (state = initialState, action) => {
 	switch(action.type) {
 		case 'to-do/add': {
-			console.log('reducer!')
 			return {
 				...state,
-				entities: [{}]
+				entities: state.entities.concat({ ...action.payload })
 			}
 		}
 	}
 	return state;
 }
 
+const ToDoItem = ({ to_do }) => {
+	return (
+		<li>{to_do.title}</li>
+	)
+}
+
 const App = () => {
+	const [value, setValue] = useState('')
 	const dispatch = useDispatch()
 	const state = useSelector(x => x)
-	console.log(state, 'rendering')
+
+	const submit = e => {
+		e.preventDefault()
+		if (!value.trim()) {
+			return
+		}
+		const id = Math.random().toString(36)
+		const to_do = { title: value, completed: false, id }
+		dispatch({ type: 'to-do/add', payload: to_do })
+		setValue('')
+	}
 
 	return (
 		<div>
-			<form>
-				<input />
+			<form onSubmit={submit}>
+				<input value={value} onChange={e => setValue(e.target.value)}/>
 			</form>
-			<button onClick={() => dispatch({ type: 'to-do/add' })}>Show TO-DOs</button>
+			<button>Show TO-DOs</button>
 			<button>Completed</button>
 			<button>Uncompleted</button>
 			<ul>
-				<li>TO-DO 1</li>
-				<li>TO-DO 2</li>
+				{state.entities.map(to_do => <ToDoItem key={to_do.id} to_do={to_do} />)}
 			</ul>
 		</div>
 	)
