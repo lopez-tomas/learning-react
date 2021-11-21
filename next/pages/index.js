229e8149fa502dadb227ccdaf336be079d58eab9
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-11-21 20:57:31 -0300

    feat: using Image component.

--- next/pages/index.js
+++ next/pages/index.js
@@ -1,10 +1,14 @@
 import Link from 'next/link'
+import Image from 'next/image'
+import Coffee from '../public/coffee.jpg'
 
+//<Image src="/coffee.jpg" width={400} height={600} />
 export default function Home() {
   return (
     <div>
       <Link href="/piggies/">Ir to Piggies</Link>
       <p>Happy piggy</p>
+      <Image src={Coffee} width={400} height={600} />
     </div>
   )
 }
