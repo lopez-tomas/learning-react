db0bf1c5e1f3245b9c10eaa48e3ef651bc80046f
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-11-28 20:22:35 -0300

    test: testing dynamic components.

--- poke/__tests__/index.test.js
+++ poke/__tests__/index.test.js
@@ -5,14 +5,22 @@ describe('Index', () => {
 
 	describe('Component', () => {
 		it("it's rendered", () => {
-			//render( -> On this way, we are calling 'screen' method. If we use only 'render', we will obtain only the elements of the rendered component and not full screen.
-			const { getByTestId } = render(
-				<Pokemons pokemons={[]}/>
+			//const { getByTestId } = render(
+			//-> On this way, we are calling 'screen' method. If we use only 'render', we will obtain only the elements of the rendered component and not full screen.
+			render( 
+				<Pokemons pokemons={[{ name: 'Happy Piggy', url: '/pokemons/detail/1' }]}/>
 			)
 
-			//const paragraph = screen.getByTestId('tittle') -> using 'screen'
-			const paragraph = getByTestId('tittle')
+			const paragraph = screen.getByTestId('tittle') //-> using 'screen'
+			//const paragraph = getByTestId('tittle')
 			expect(paragraph).toBeInTheDocument()
+
+			const piggy = screen.getByText('Happy Piggy')
+			//console.log(piggy.getAttribute('href'))
+
+			expect(piggy).toBeInTheDocument()
+			const url = piggy.getAttribute('href')
+			expect(url).toEqual('/pokemons/1')
 		})
 	})
 
