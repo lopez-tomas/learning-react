648cebfc2a75930f8cb47434306954813512ac56
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-11-28 20:12:13 -0300

    test: rendering components.

--- poke/__tests__/index.test.js
+++ poke/__tests__/index.test.js
@@ -1,16 +1,21 @@
-const add = () => 2 + 2
+import { render, screen } from '@testing-library/react'
+import Pokemons from '../pages/index'
 
 describe('Index', () => {
 
 	describe('Component', () => {
 		it("it's rendered", () => {
+			//render( -> On this way, we are calling 'screen' method. If we use only 'render', we will obtain only the elements of the rendered component and not full screen.
+			const { getByTestId } = render(
+				<Pokemons pokemons={[]}/>
+			)
 
+			//const paragraph = screen.getByTestId('tittle') -> using 'screen'
+			const paragraph = getByTestId('tittle')
+			expect(paragraph).toBeInTheDocument()
 		})
 	})
 
 	describe('getStaticProps', () => {
-		it('add 2 + 2', () => {
-			expect(add()).toBe(4)
-		})
 	})
 })
