11ad6f749037970aa2891201d92280ad9dfe13e1
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-11-29 17:08:18 -0300

    feat: styles moved from '[id].js' file to this new one. Styles are exported and used.

---
+++ poke/styles/pokemon.js
@@ -0,0 +1,93 @@
+export const styles = {
+	name: {
+		display: 'inline',
+		fontFamily: 'sans-serif',
+		border: '1px solid black',
+		borderRadius: '5px',
+		padding: '5px',
+	},
+	id: {
+		fontFamily: 'sans-serif',
+		fontStyle: 'italic',
+		color: '#ccc',
+	}
+}
+
+export const types_styles = {
+	color: '#fff',
+
+	normal: {
+		backgroundColor: '#a4acaf',
+	},
+
+	fire: {
+		backgroundColor: '#fd7d24',
+	},
+
+	water: {
+		backgroundColor: '#4592c4',
+	},
+
+	grass: {
+		backgroundColor: '#9bcc50',
+	},
+
+	electric: {
+		backgroundColor: '#eed535',
+	},
+
+	ice: {
+		backgroundColor: '#51c4e7',
+	},
+
+	fighting: {
+		backgroundColor: '#d56723',
+	},
+
+	poison: {
+		backgroundColor: '#b97fc9',
+	},
+
+	ground: {
+		background: 'linear-gradient(180deg, #f7de3f 50%, #ab9842 50%)',
+		backgroundColor: '#f7de3f',
+	},
+
+	flying: {
+		background: 'linear-gradient(180deg, #3dc7ef 50%, #bdb9b8 50%)',
+		backgroundColor: '#3dc7ef',
+	},
+
+	psychic: {
+		backgroundColor: '#f366b9',
+	},
+
+	bug: {
+		backgroundColor: '#729f3f',
+	},
+
+	rock: {
+		backgroundColor: '#a38c21',
+	},
+
+	ghost: {
+		backgroundColor: '#7b62a3',
+	},
+
+	dark: {
+		backgroundColor: '#707070',
+	},
+
+	dragon: {
+		background: 'linear-gradient(180deg, #53a4cf 50%, #bdb9b8 50%)',
+		backgroundColor: '#53a4cf',
+	},
+
+	steel: {
+		backgroundColor: '#9eb7b8',
+	},
+
+	fairy: {
+		backgroundColor: '#fdb9e9',
+	}
+}
