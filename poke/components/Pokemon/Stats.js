f9b1930c2f88694fb12045ebf177b188f0a7d4b4
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-12-01 21:40:55 -0300

    feat: minor changes.

--- poke/components/Pokemon/Stats.js
+++ poke/components/Pokemon/Stats.js
@@ -2,10 +2,18 @@ import { fixName } from '../../pages/pokemon/[id]'
 
 const Stats = ({ stats }) => {
 	return (
-		<div>
+		<div className="stats__container">
 			<h1>Base Stats</h1>
 			<ul>
-				{stats.map(stat => <li key={stat.stat.name}>{fixName(stat.stat.name)}: {stat.base_stat}</li>)}
+				{stats.map(stat =>
+					<li
+						className="stat-item"
+						key={stat.stat.name}
+					>
+						<span className="stat-item__name">{fixName(stat.stat.name)}: </span> 
+						<span className="stat-item__value">{stat.base_stat}</span>
+					</li>
+				)}
 			</ul>
 		</div>
 	)
