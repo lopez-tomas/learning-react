3b9bc2089c8f29b79c9c5aabf556eddb2ba72c92
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-12-03 00:04:38 -0300

    feat: typeStyles() function created. It receives a type name and returns an object with global & given type styles. It's used to apply styles to Pokémon Type(s).

--- poke/components/Pokemon/Types.js
+++ poke/components/Pokemon/Types.js
@@ -1,34 +1,73 @@
 import { fixName } from '../../pages/pokemon/[id]'
 import { types_styles } from '../../styles/pokemon'
 
+const typeStyles = (type) => {
+	switch(type) {
+		case 'normal':
+			return { ...types_styles.default, ...types_styles.normal }
+
+		case 'fire':
+			return { ...types_styles.default, ...types_styles.fire }
+
+		case 'water':
+			return { ...types_styles.default, ...types_styles.water }
+
+		case 'grass':
+			return { ...types_styles.default, ...types_styles.grass }
+
+		case 'electric':
+			return { ...types_styles.default, ...types_styles.electric }
+
+		case 'ice':
+			return { ...types_styles.default, ...types_styles.ice }
+
+		case 'fighting':
+			return { ...types_styles.default, ...types_styles.fighting }
+
+		case 'poison':
+			return { ...types_styles.default, ...types_styles.poison }
+
+		case 'ground':
+			return { ...types_styles.default, ...types_styles.ground }
+
+		case 'flying':
+			return { ...types_styles.default, ...types_styles.flying }
+
+		case 'psychic':
+			return { ...types_styles.default, ...types_styles.psychic }
+
+		case 'bug':
+			return { ...types_styles.default, ...types_styles.bug }
+
+		case 'rock':
+			return { ...types_styles.default, ...types_styles.rock }
+
+		case 'ghost':
+			return { ...types_styles.default, ...types_styles.ghost }
+
+		case 'dark':
+			return { ...types_styles.default, ...types_styles.dark }
+
+		case 'dragon':
+			return { ...types_styles.default, ...types_styles.dragon }
+
+		case 'steel':
+			return { ...types_styles.default, ...types_styles.steel }
+
+		case 'fairy':
+			return { ...types_styles.default, ...types_styles.fairy }
+
+		default:
+			return { ...types_styles.default, ...types_styles.noType }
+	}
+}
+
 const Types = ({ types }) => {
 	return (
 		<div className="type__container">
 			{types.map(type =>
 				<div key={type.type.name}>
-					<p
-						style={{...types_styles,
-							...type.type.name === 'normal' 	? types_styles.normal 	:
-							type.type.name === 'fire' 			? types_styles.fire 		:
-							type.type.name === 'water' 			? types_styles.water 		:
-							type.type.name === 'grass' 			? types_styles.grass 		:
-							type.type.name === 'electric' 	? types_styles.electric :
-							type.type.name === 'ice' 				? types_styles.ice 			:
-							type.type.name === 'fighting' 	? types_styles.fighting :
-							type.type.name === 'poison' 		? types_styles.poison 	:
-							type.type.name === 'ground' 		? types_styles.ground 	:
-							type.type.name === 'flying' 		? types_styles.flying 	:
-							type.type.name === 'psychic' 		? types_styles.psychic 	:
-							type.type.name === 'bug' 				? types_styles.bug 			:
-							type.type.name === 'rock' 			? types_styles.rock 		:
-							type.type.name === 'ghost' 			? types_styles.ghost 		:
-							type.type.name === 'dark' 			? types_styles.dark 		:
-							type.type.name === 'dragon' 		? types_styles.dragon 	:
-							type.type.name === 'steel' 			? types_styles.steel 		:
-							type.type.name === 'fairy' 			? types_styles.fairy 		:
-							null
-						}}
-					>
+					<p style={typeStyles(type.type.name)}>
 						{fixName(type.type.name)}
 					</p>
 				</div>
